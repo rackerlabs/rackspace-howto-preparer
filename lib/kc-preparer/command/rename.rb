f4527fbc90dd43698eb6280cb60d8567e7a9713d
atarola <anthony.tarola@gmail.com>
2016-01-04 15:34:02 -0600

    making sure the old_doc is not an array

--- lib/kc-preparer/command/rename.rb
+++ lib/kc-preparer/command/rename.rb
@@ -6,11 +6,13 @@ class KCPreparer::RenameCommand < KCPreparer::Command
     # two parents, but only one of those commits should actually have the file
     parents = @commit['parents'].map { |parent| parent['sha'] }
 
-    old_doc = parents.find do |sha|
+    old_doc = parents.map do |sha|
       KCPreparer::Github.get_file_at_sha(@config, @change['previous_filename'], sha)
     end
 
-    if old_doc
+    puts old_doc.compact.first
+
+    unless old_doc.compact.first.nil?
       document = KCPreparer::Document.new(@config, old_doc)
       KCPreparer::Nexus.delete_doc(@config, document)
     end
